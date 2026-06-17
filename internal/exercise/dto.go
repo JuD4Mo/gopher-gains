6e85fef66b8cb9ea8ff832aa594af73823647c2a
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-06-17 01:37:19 -0500

    fix(exercise): add min=1,max=50 validation tags to Description field

--- internal/exercise/dto.go
+++ internal/exercise/dto.go
@@ -5,14 +5,14 @@ import "github.com/JuD4Mo/gopher-gains/pkg/validation"
 type (
 	CreateExerciseDto struct {
 		Name              string `json:"name" validate:"required,min=1,max=50"`
-		Description       string `json:"description" validate:"required"`
+		Description       string `json:"description" validate:"required,min=1,max=50"`
 		ExecutionTip      string `json:"executionTip" validate:"required"`
 		TargetMuscleGroup string `json:"muscleGroup" validate:"required,oneof=chest back legs arms delts abs"`
 	}
 
 	UpdateExerciseDto struct {
 		Name              *string `json:"name" validate:"omitempty,min=1,max=50"`
-		Description       *string `json:"description" validate:"omitempty,min=1"`
+		Description       *string `json:"description" validate:"omitempty,min=1,max=50"`
 		ExecutionTip      *string `json:"executionTip" validate:"omitempty,min=1"`
 		TargetMuscleGroup *string `json:"muscleGroup" validate:"omitempty,oneof=chest back legs arms delts abs"`
 	}
