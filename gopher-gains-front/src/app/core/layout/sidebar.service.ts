df05c85d1e307d611812cdfb546af6c525ccb1da
JuD4Mo <juan_d.munoz_o@uao.edu.co>
2026-06-23 00:59:29 -0500

    chore: moving frontend to another repo

--- gopher-gains-front/src/app/core/layout/sidebar.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, signal } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class SidebarService {
-  readonly open = signal(false);
-
-  toggle() { this.open.update(v => !v); }
-  close() { this.open.set(false); }
-}
